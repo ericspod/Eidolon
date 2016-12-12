e8b501cbbce71fa0f2da0eb0607d6a73dd45960a
Eric <afsdfsadf@nowhere.com>
2016-12-12 17:01:31 +0000

    Updates

--- tutorial/tutorial7.py
+++ tutorial/tutorial7.py
@@ -41,15 +41,15 @@ mgr.controller.setRotation(1.0,1.0) # rotate the camera slightly
 m=mgr.getMaterial('Rainbow') # acquire this pre-existing material
  
 # create an isosurface set using the field `df' as the input and generating 10 planes roughly between the field's minimum and maxmimum values
-rep=obj.createRepr(ReprType._isosurf,0,field=df,numitervals=10,minv=0.0,maxv=80.0,valfunc=ValueFunc.Magnitude)
+rep=obj.createRepr(ReprType._isosurf,0,field=df.getName(),numitervals=10,minv=0.0,maxv=80.0,valfunc=ValueFunc._Magnitude)
 mgr.addSceneObjectRepr(rep)
 
-rep.applyMaterial(m,field=df)
+rep.applyMaterial(m,field=df.getName())
 
 # do the same with isolines but at a higher refinement value
-rep=obj.createRepr(ReprType._isoline,5,field=df,numitervals=10,minv=0.0,maxv=80.0,radius=0.5,valfunc=ValueFunc.Magnitude)
+rep=obj.createRepr(ReprType._isoline,5,field=df.getName(),numitervals=10,minv=0.0,maxv=80.0,radius=0.5,valfunc=ValueFunc._Magnitude)
 mgr.addSceneObjectRepr(rep)
 
-rep.applyMaterial(m,field=df)
+rep.applyMaterial(m,field=df.getName())
 
 
