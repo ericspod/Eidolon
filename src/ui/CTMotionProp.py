d71b4a288b8d735ff8fca82ca6f12289f930175a
Eric Kerfoot <eric.kerfoot@gmail.com>
2017-04-06 20:50:28 +0100

    updates

--- src/ui/CTMotionProp.py
+++ src/ui/CTMotionProp.py
@@ -2,8 +2,7 @@
 
 # Form implementation generated from reading ui file 'src/ui/CTMotionProp.ui'
 #
-# Created: Mon Apr  3 19:06:19 2017
-#      by: PyQt4 UI code generator 4.10.4
+# Created by: PyQt4 UI code generator 4.11.4
 #
 # WARNING! All changes made in this file will be lost!
 
