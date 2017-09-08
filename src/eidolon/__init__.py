8becc43a2338149083c67bf01d80ef54918de4dd
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2017-09-08 16:53:50 +0100

    Updates

--- src/eidolon/__init__.py
+++ src/eidolon/__init__.py
@@ -36,7 +36,7 @@ from eidolon.SceneComponents import *
 from eidolon.Application import *
 
 __appname__='Eidolon Biomedical Framework'
-__version_info__=(0,3,99) # global application version, major/minor/patch, patch value 99 means development code directly from the repo
+__version_info__=(0,4,0) # global application version, major/minor/patch, patch value 99 means development code directly from the repo
 __version__='%i.%i.%i'%__version_info__
 __author__='Eric Kerfoot'
 __copyright__="Copyright (c) 2016-7 Eric Kerfoot, King's College London, all rights reserved. Licensed under the GPL (see LICENSE.txt)."
