ee9da7c9266ae7b724ef80376e52ad610bde57ff
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2018-02-21 13:26:50 +0000

    Updates

--- src/ui/Resources_rc4.py
+++ src/ui/Resources_rc4.py
@@ -2,7 +2,8 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt4 (Qt v4.8.7)
+# Created: Wed Feb 21 13:22:01 2018
+#      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
 
