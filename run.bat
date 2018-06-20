6d95cf86b090d6d257ef4a5a838214d35a345e11
Eric Kerfoot <eric.kerfoot@gmail.com>
2018-06-20 17:46:20 +0100

    Py3 recompile stuff

--- run.bat
+++ run.bat
@@ -4,13 +4,13 @@ SET APPDIR=%~dp0
 
 :: Pull out the Python install path we're using from the registry, if this gets the wrong value when there's multiple
 :: Python installs, you'll have to set InstallPath manually
-FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY HKCU\Software\Python\PythonCore\2.7\InstallPath 2^>NUL') DO  SET InstallPath=%%B
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY HKCU\Software\Python\PythonCore\3.6\InstallPath 2^>NUL') DO  SET InstallPath=%%B
 
 :: If the path wasn't set for the current user, check the local machine
-IF "%InstallPath%" == "" FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY HKLM\SOFTWARE\Python\PythonCore\2.7\InstallPath 2^>nul') DO SET InstallPath=%%B
+IF "%InstallPath%" == "" FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY HKLM\SOFTWARE\Python\PythonCore\3.6\InstallPath 2^>nul') DO SET InstallPath=%%B
 
 :: Choose a default value if neither registry key is present
-IF [%InstallPath%] == [] SET InstallPath=C:\Python27\
+IF [%InstallPath%] == [] SET InstallPath=C:\Python36\
 
 :: PATH is needed for the DLL the render links to and the renderer plugin DLLs
 SET PATH=%APPDIR%EidolonLibs\win64_mingw\bin;%PATH%
